b2817623e3b35c5afa2feca4d6c4fde5ac39a4ab
Leiniao1 <yinhangtsinghua@gmail.com>
2015-07-22 13:43:00 -0700

    Update the clickon function
    Game still not functional

--- Evo3/app/src/main/java/com/example/hyin/evo3/MainActivity.java
+++ Evo3/app/src/main/java/com/example/hyin/evo3/MainActivity.java
@@ -1,17 +1,58 @@
 package com.example.hyin.evo3;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.view.View;
+import java.lang.String;
 
 public class MainActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Generation2(View v){
+        ImageButton IBmain = (ImageButton) findViewById(R.id.imageButton);
+        ImageButton IBsub = (ImageButton) findViewById(R.id.imageButton2);
+        TextView txV2 = (TextView) findViewById(R.id.textView2);
+        String s = (String)txV2.getText();
+        s = s.toLowerCase();
+        Drawable Pic1 = getDrawable(getResources()
+                .getIdentifier(s, "drawable", getPackageName()));
+        IBmain.setBackground(Pic1);
+        return;
+    }
+
+    public void Generation3(View v){
+        ImageButton IBmain = (ImageButton) findViewById(R.id.imageButton);
+        ImageButton IBsub = (ImageButton) findViewById(R.id.imageButton3);
+        TextView txV3 = (TextView) findViewById(R.id.textView3);
+        String s = (String)txV3.getText();
+        s = s.toLowerCase();
+        Drawable Pic1 = getDrawable(getResources()
+                .getIdentifier(s, "drawable", getPackageName()));
+        IBmain.setBackground(Pic1);
+        return;
+    }
+
+    public void Generation4(View v){
+        ImageButton IBmain = (ImageButton) findViewById(R.id.imageButton);
+        ImageButton IBsub = (ImageButton) findViewById(R.id.imageButton4);
+        TextView txV4 = (TextView) findViewById(R.id.textView4);
+        String s = (String)txV4.getText();
+        s = s.toLowerCase();
+        Drawable Pic1 = getDrawable(getResources()
+                .getIdentifier(s, "drawable", getPackageName()));
+        IBmain.setBackground(Pic1);
+        return;
     }
 
     @Override
